da660e5a99d889c58f437f4421fdcb43c5ca2133
TSG <TSG@TSGAC>
2021-03-19 10:49:40 -0400

    Add project files.

---
+++ LightCycleEnergy/LightCycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LightCycleEnergy
+{
+    class LightCycle
+    {
+        static void Main()
+        {
+            //LightAmp object/Instance
+            LightMap lightmap = new LightMap();
+        }
+    }
+}
